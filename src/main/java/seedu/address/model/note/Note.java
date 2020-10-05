03b30ec1d2283590552dec2d1d65bdef2876fcf6
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-10-05 18:42:44 +0800

    Create walking skeleton

---
+++ src/main/java/seedu/address/model/note/Note.java
@@ -0,0 +1,28 @@
+package seedu.address.model.note;
+
+public class Note {
+    // TODO: Add implementation
+//    private final String noteContents;
+//
+//    public Note(String content) {
+//        requireNonNull(content)
+//        noteContents = content;
+//    }
+//    @Override
+//    public String toString() {
+//        return noteContents;
+//    }
+//    @Override
+//    public boolean equals(Object other) {
+//        if (other == this) {
+//            return true;
+//        }
+//        if (!(other instanceof Note)) {
+//            return false;
+//        }
+//    }
+//    @Override
+//    public int hashCode() {
+//
+//    }
+}
