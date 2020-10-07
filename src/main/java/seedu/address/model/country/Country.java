fd1ec53592d623f4e2e7f5432215099abcb9390a
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-08 03:42:30 +0800

    Update implementation of Country to use a Map based on java Locale class

--- src/main/java/seedu/address/model/country/Country.java
+++ src/main/java/seedu/address/model/country/Country.java
@@ -5,36 +5,59 @@ import static java.util.Objects.requireNonNull;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import seedu.address.model.note.Note;
 
-public enum Country {
-    SINGAPORE("Singapore", "SG", new ArrayList<>());
+/**
+ * A representation of a Country that can be identified by a 2-letter ISO3166 country-code or by its country
+ * name. It contains a list of country notes.
+ */
+public class Country {
+    private final String countryName;
+    private final String countryCode;
+    private final ArrayList<Note> countryNotes;
 
-    private String countryName;
-    private String countryCode;
-    private ArrayList<Note> countryNotes;
-
-    Country(String countryName, String countryCode, ArrayList<Note> countryNotes) {
-        this.countryName = countryName;
+    /**
+     * Initializes a Country by its countryCode.
+     * @param countryCode The ISO3166 2-letter country code of the country to be initialized.
+     */
+    public Country(String countryCode) {
         this.countryCode = countryCode;
-        this.countryNotes = countryNotes;
+        this.countryName = new Locale("", countryCode).getDisplayName();
+        this.countryNotes = new ArrayList<>();
     }
 
-    public static Country countryCodeToCountry(String countryCode) {
-        for (Country c : Country.values()) {
-            System.out.println(c);
-        }
-        return null;
-    }
-
+    /**
+     * Gets the list of country notes associated with this country.
+     * @return The list of country notes associated with this country.
+     */
     public List<Note> getCountryNotes() {
         return Collections.unmodifiableList(this.countryNotes);
     }
 
+    /**
+     * Adds a country note for this country.
+     * @param countryNote The country note to be added.
+     */
     public void addCountryNote(Note countryNote) {
         requireNonNull(countryNote);
         this.countryNotes.add(countryNote);
     }
 
+    /**
+     * Gets the country name of this country.
+     * @return The country name of this country.
+     */
+    public String getCountryName() {
+        return countryName;
+    }
+
+    /**
+     * Gets the country code of this country.
+     * @return The country code of this country.
+     */
+    public String getCountryCode() {
+        return countryCode;
+    }
 }
---
+++ src/main/java/seedu/address/model/country/CountryManager.java
@@ -0,0 +1,42 @@
+package seedu.address.model.country;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+/**
+ * A high-level class responsible for mapping ISO3166 2-letter country codes to countries.
+ */
+public class CountryManager {
+    //TODO: Should include checking for 3-letter Country Code?
+    private final HashMap<String, Country> countryCodeMap;
+
+    /**
+     * Initializes a CountryManager with a Map that maps ISO3166 2-letter country codes to countries.
+     */
+    public CountryManager() {
+        countryCodeMap = initCountryCodeMap();
+    }
+
+    /**
+     * Returns the country that is identified by the given country code.
+     * @param countryCode The ISO3166 2-letter country code of the particular country.
+     * @return The country that is identified by the given country code.
+     */
+    public Country getCountryFromCode(String countryCode) {
+        if (countryCodeMap.containsKey(countryCode)) {
+            return countryCodeMap.get(countryCode);
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    private static HashMap<String, Country> initCountryCodeMap() {
+        HashMap<String, Country> countryCodeMap = new HashMap<>();
+        String[] countryCodes = Locale.getISOCountries();
+        for (String countryCode : countryCodes) {
+            countryCodeMap.put(countryCode, new Country(countryCode));
+        }
+        return countryCodeMap;
+    }
+
+}
