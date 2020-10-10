127bb87a7eabf26217917c9d6d569ea65e9f119c
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-11 04:05:15 +0800

    Fix failing tests

--- src/main/java/seedu/address/logic/commands/CountryNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/CountryNoteCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.logic.commands.ClientEditCommand.EditClientDescriptor;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.country.Country;
--- src/main/java/seedu/address/model/country/Country.java
+++ src/main/java/seedu/address/model/country/Country.java
@@ -72,4 +72,34 @@ public class Country {
         return countryCode;
     }
 
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Country)) {
+            return false;
+        }
+
+        // state check
+        Country c = (Country) other;
+
+        for (Note note: countryNotes) {
+            if (!c.countryNotes.contains(note)) {
+                return false;
+            }
+        }
+
+        for (Note note: c.countryNotes) {
+            if (!countryNotes.contains(note)) {
+                return false;
+            }
+        }
+
+        return countryCode.equals(c.countryCode) && countryName.equals(c.countryName);
+    }
+
 }
