4f33aaa335a096644d8cf0bad56289bf28e13851
tankangliang <tankangliang98@gmail.com>
2020-10-11 20:02:56 +0800

    Fix PR issues

--- src/test/java/seedu/address/logic/parser/ClientAddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ClientAddCommandParserTest.java
@@ -72,7 +72,7 @@ public class ClientAddCommandParserTest {
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_AMY
                 + ADDRESS_DESC_BOB + COUNTRY_DESC_BOB + TAG_DESC_FRIEND, new ClientAddCommand(expectedClient));
 
-        // multiple countries - last countries
+        // multiple countries - last country accepted
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
                 + COUNTRY_DESC_AMY + COUNTRY_DESC_BOB + TAG_DESC_FRIEND, new ClientAddCommand(expectedClient));
 
