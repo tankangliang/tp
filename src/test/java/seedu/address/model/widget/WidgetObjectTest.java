e869031b6133a8fb37e884a944fa1e4fc05d362a
LeeEnHao <lienhao.le@gmail.com>
2020-10-11 15:19:51 +0800

    Fixed test names

--- src/test/java/seedu/address/model/widget/WidgetObjectTest.java
+++ src/test/java/seedu/address/model/widget/WidgetObjectTest.java
@@ -8,14 +8,14 @@ import org.junit.jupiter.api.Test;
 class WidgetObjectTest {
 
     @Test
-    void test_displayed_fields_proper_truncate_when_over_limit() {
+    void testDisplayedFieldsProperTruncateWhenOverLimit() {
         WidgetObject wo = new WidgetObject();
         wo.set("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16");
         assertEquals(wo.footer().orElse(""), "13");
     }
 
     @Test
-    void test_Equals() {
+    void testEquals() {
         WidgetObject o1 = new WidgetObject();
         o1.set("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16");
         WidgetObject o2 = new WidgetObject();
@@ -24,11 +24,11 @@ class WidgetObjectTest {
     }
 
     @Test
-    void test_Not_Equals() {
+    void testNotEquals() {
         WidgetObject o1 = new WidgetObject();
         o1.set("1");
         WidgetObject o2 = new WidgetObject();
-        o2.set("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16");
+        o2.set("1", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16");
         assertNotEquals(o1, o2);
     }
 }
