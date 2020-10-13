638a03ce30f0b9ccf652d2343684dccac080ac97
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-13 20:14:22 +0800

    Refactor countryManager to store countryNotes

--- src/main/java/seedu/address/model/country/Country.java
+++ src/main/java/seedu/address/model/country/Country.java
@@ -1,13 +1,6 @@
 package seedu.address.model.country;
 
-import static java.util.Objects.requireNonNull;
-
-import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.Locale;
-import java.util.Set;
-
-import seedu.address.model.note.Note;
 
 /**
  * A representation of a Country that can be identified by a 2-letter ISO3166 country-code or by its country
@@ -17,7 +10,6 @@ public class Country {
 
     private final String countryName;
     private final String countryCode;
-    private final Set<Note> countryNotes;
 
     /**
      * Initializes a Country by its countryCode.
@@ -27,37 +19,6 @@ public class Country {
     public Country(String countryCode) {
         this.countryCode = countryCode;
         this.countryName = new Locale("", countryCode).getDisplayName();
-        this.countryNotes = new LinkedHashSet<>();
-    }
-
-    /**
-     * Gets the list of country notes associated with this country.
-     *
-     * @return The list of country notes associated with this country.
-     */
-    public Set<Note> getCountryNotes() {
-        return Collections.unmodifiableSet(this.countryNotes);
-    }
-
-    /**
-     * Adds a country note for this country.
-     * Method is protected as it is not intended for use outside of this package.
-     *
-     * @param countryNote The country note to be added.
-     */
-    protected void addCountryNote(Note countryNote) {
-        requireNonNull(countryNote);
-        this.countryNotes.add(countryNote);
-    }
-
-    /**
-     * Checks whether given country note has been added for this country.
-     *
-     * @param countryNote The given country note.
-     * @return True if given country note has been added for this country, false otherwise.
-     */
-    protected boolean hasCountryNote(Note countryNote) {
-        return countryNotes.contains(countryNote);
     }
 
     /**
