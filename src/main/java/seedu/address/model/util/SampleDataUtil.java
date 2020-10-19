4787ab2d2ea5c97327dd01def6de706bbf35bfc7
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-19 17:15:57 +0800

    Add sample country notes data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,9 +1,11 @@
 package seedu.address.model.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import seedu.address.commons.util.PairUtil;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.client.Address;
@@ -13,33 +15,46 @@ import seedu.address.model.client.Name;
 import seedu.address.model.client.Phone;
 import seedu.address.model.client.Timezone;
 import seedu.address.model.country.Country;
+import seedu.address.model.note.Note;
 import seedu.address.model.tag.Tag;
 
 /**
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
 public class SampleDataUtil {
+
     public static Client[] getSampleClients() {
-        return new Client[] {
-            new Client(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"), new Country("SG"), new Timezone("GMT+8"),
-                getTagSet("friends")),
-            new Client(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Country("SG"),
-                new Timezone("GMT+8"), getTagSet("colleagues", "friends")),
-            new Client(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Country("SG"), new Timezone("GMT+8"),
-                getTagSet("neighbours")),
-            new Client(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Country("SG"),
-                new Timezone("GMT+8"), getTagSet("family")),
-            new Client(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"), new Country("SG"), new Timezone("GMT+8"),
-                getTagSet("classmates")),
-            new Client(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"), new Country("SG"), new Timezone("GMT+8"),
-                getTagSet("colleagues"))
+        return new Client[]{
+                new Client(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                        new Address("Blk 30 Geylang Street 29, #06-40"), new Country("SG"),
+                        new Timezone("GMT+8"),
+                        getTagSet("friends")),
+                new Client(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Country("SG"),
+                        new Timezone("GMT+8"), getTagSet("colleagues", "friends")),
+                new Client(new Name("Charlotte Oliveiro"), new Phone("93210283"),
+                        new Email("charlotte@example.com"),
+                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Country("SG"),
+                        new Timezone("GMT+8"),
+                        getTagSet("neighbours")),
+                new Client(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Country("SG"),
+                        new Timezone("GMT+8"), getTagSet("family")),
+                new Client(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+                        new Address("Blk 47 Tampines Street 20, #17-35"), new Country("SG"),
+                        new Timezone("GMT+8"),
+                        getTagSet("classmates")),
+                new Client(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+                        new Address("Blk 45 Aljunied Street 85, #11-31"), new Country("SG"),
+                        new Timezone("GMT+8"),
+                        getTagSet("colleagues"))
         };
+    }
+
+    public static ArrayList<PairUtil<Country, Note>> getSampleCountryNotes() {
+        return new ArrayList<>(Arrays.asList(
+                new PairUtil<>(new Country("SG"), new Note("this is a note")),
+                new PairUtil<>(new Country("MY"), new Note("this is another note"))));
     }
 
     public static ReadOnlyAddressBook getSampleAddressBook() {
@@ -47,6 +62,11 @@ public class SampleDataUtil {
         for (Client sampleClient : getSampleClients()) {
             sampleAb.addClient(sampleClient);
         }
+
+        for (PairUtil<Country, Note> countryNote : getSampleCountryNotes()) {
+            sampleAb.addCountryNote(countryNote.getFirst(), countryNote.getSecond());
+        }
+
         return sampleAb;
     }
 
