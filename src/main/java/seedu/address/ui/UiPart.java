5ba70ae14b99ae000238deb35eb30741b603b827
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-10-24 14:53:19 +0800

    Fix CI

--- src/main/java/seedu/address/ui/UiPart.java
+++ src/main/java/seedu/address/ui/UiPart.java
@@ -12,7 +12,7 @@ import seedu.address.MainApp;
  * Represents a distinct part of the UI. e.g. Windows, dialogs, panels, status bars, etc.
  * It contains a scene graph with a root node of type {@code T}.
  */
-public class UiPart<T> {
+public abstract class UiPart<T> {
 
     /** Resource folder where FXML files are stored. */
     public static final String FXML_FILE_FOLDER = "/view/";
