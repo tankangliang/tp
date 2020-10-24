81f7dfd95190d68b670c2ff21e6c4637dbd2ab45
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-10-24 18:55:46 +0800

    Update MainWindowCloseTest.java

--- src/test/java/seedu/address/ui/MainWindowCloseTest.java
+++ src/test/java/seedu/address/ui/MainWindowCloseTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.io.TempDir;
 import org.testfx.api.FxToolkit;
 
 import guitests.guihandles.HelpWindowHandle;
-import guitests.guihandles.StageHandle;
+import guitests.guihandles.MainWindowHandle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import seedu.address.MainApp;
@@ -26,11 +26,11 @@ import seedu.address.storage.StorageManager;
  * Contains tests for closing of the {@code MainWindow}.
  */
 public class MainWindowCloseTest extends GuiUnitTest {
+
     @TempDir
     public Path temporaryFolder;
-
     private MainWindow mainWindow;
-    private EmptyMainWindowHandle mainWindowHandle;
+    private MainWindowHandle mainWindowHandle;
     private Stage stage;
 
     @BeforeEach
@@ -43,7 +43,7 @@ public class MainWindowCloseTest extends GuiUnitTest {
             this.stage = stage;
             mainWindow = new MainWindow(stage, new LogicManager(new ModelManager(), storageManager), new MainApp());
             mainWindow.fillInnerParts();
-            mainWindowHandle = new EmptyMainWindowHandle(stage);
+            mainWindowHandle = new MainWindowHandle(stage);
             mainWindowHandle.focus();
         });
         FxToolkit.showStage();
@@ -51,7 +51,10 @@ public class MainWindowCloseTest extends GuiUnitTest {
 
     @Test
     public void close_menuBarExitButton_allWindowsClosed() {
-        mainWindowHandle.clickOnMenuExitButton();
+        guiRobot.pauseForHuman();
+        assertTrue(mainWindowHandle.isShowing());
+        guiRobot.clickOn("#file");
+        guiRobot.clickOn("#exit");
         guiRobot.pauseForHuman();
         // The application will exit when all windows are closed.
         assertEquals(Collections.emptyList(), guiRobot.listWindows());
@@ -59,45 +62,14 @@ public class MainWindowCloseTest extends GuiUnitTest {
 
     @Test
     public void close_externalRequest_exitAppRequestEventPosted() {
-        mainWindowHandle.clickOnMenuHelpButton();
+        guiRobot.clickOn("#help");
+        guiRobot.clickOn("#helpMenuItem");
         assertTrue(HelpWindowHandle.isWindowPresent());
-        mainWindowHandle.closeMainWindowExternally();
+        // Close the main window
+        guiRobot.interact(() -> stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST)));
         // The application will exit when all windows are closed.
         guiRobot.pauseForHuman();
         assertEquals(Collections.emptyList(), guiRobot.listWindows());
     }
 
-    /**
-     * A handle for an empty {@code MainWindow}. The components in {@code MainWindow} are not initialized.
-     */
-    private class EmptyMainWindowHandle extends StageHandle {
-
-        private EmptyMainWindowHandle(Stage stage) {
-            super(stage);
-        }
-
-        /**
-         * Closes the {@code MainWindow} by clicking on the menu bar's exit button.
-         */
-        private void clickOnMenuExitButton() {
-            guiRobot.clickOn("#file");
-            guiRobot.clickOn("#exit");
-        }
-
-        /**
-         * Closes the {@code MainWindow} through an external request {@code MainWindow} (e.g pressing the 'X' button on
-         * the {@code MainWindow} or closing the app through the taskbar).
-         */
-        private void closeMainWindowExternally() {
-            guiRobot.interact(() -> stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST)));
-        }
-
-        /**
-         * Opens the {@code HelpWindow} by clicking on the menu bar's help button.
-         */
-        private void clickOnMenuHelpButton() {
-            guiRobot.clickOn("#help");
-            guiRobot.clickOn("#helpMenuItem");
-        }
-    }
 }
