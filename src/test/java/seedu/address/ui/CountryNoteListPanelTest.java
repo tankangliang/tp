2ada7700e80c8857db359f0239478e885f819a1d
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-10-27 10:51:53 +0800

    Fix checkstyle

--- src/test/java/seedu/address/ui/CountryNoteListPanelTest.java
+++ src/test/java/seedu/address/ui/CountryNoteListPanelTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.model.country.Country;
-
 import seedu.address.model.note.CountryNote;
 
 public class CountryNoteListPanelTest extends GuiUnitTest {
