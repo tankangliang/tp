0379b9b6fb9bc8fda76f9e108c98e1153b30c237
LeeEnHao <lienhao.le@gmail.com>
2020-10-29 18:38:15 +0800

    Update

--- src/test/java/seedu/address/ui/MainWindowTest.java
+++ src/test/java/seedu/address/ui/MainWindowTest.java
@@ -3,8 +3,10 @@ package seedu.address.ui;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.address.logic.commands.ClientViewCommand.MESSAGE_VIEW_CLIENT_SUCCESS;
 import static seedu.address.ui.HelpWindow.USERGUIDE_URL;
 
 import java.awt.Toolkit;
@@ -21,6 +23,7 @@ import guitests.guihandles.HelpWindowHandle;
 import guitests.guihandles.MainWindowHandle;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 import seedu.address.MainApp;
 import seedu.address.logic.LogicManager;
@@ -80,7 +83,7 @@ public class MainWindowTest extends GuiUnitTest {
         guiRobot.pauseForHuman();
         assertFalse(guiRobot.isWindowShown(HelpWindowHandle.HELP_WINDOW_TITLE));
 
-        // TODO: Command Execution test
+        // command execution by robot
         terminal.inputCommand("client add n/Lim p/18002345 e/lim@gmail.com a/Yishun c/SG tz/GMT+8");
         terminal.inputCommand("client add n/Kim p/18002346 e/kim@gmail.com a/Kishun c/SG tz/GMT+8");
         terminal.inputCommand("client add n/Sim p/18002347 e/sim@gmail.com a/Sishun c/SG tz/GMT+8");
@@ -90,6 +93,9 @@ public class MainWindowTest extends GuiUnitTest {
         terminal.inputCommand("client note add 2 t/reminder nt/homework submission");
         terminal.inputCommand("country note add c/SG nt/shamaladingdong");
         terminal.inputCommand("country note add c/SG nt/chingchangchongdingdong");
+
+        // skips the rest if headless: resultDisplay and view box does not update in headless.
+        assumeFalse(guiRobot.isHeadlessMode());
 
         // unknown command
         terminal.inputCommand("client viewf");
@@ -102,14 +108,16 @@ public class MainWindowTest extends GuiUnitTest {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, ClientViewCommand.MESSAGE_USAGE);
         assertEquals(expectedMessage, resultMessage);
 
+        // some valid command usage
         terminal.inputCommand("country note add");
         String resultMessage2 = getResultMessage();
         String expectedMessage2 = String.format(MESSAGE_INVALID_COMMAND_FORMAT, CountryNoteAddCommand.MESSAGE_USAGE);
         assertEquals(expectedMessage2, resultMessage2);
 
-
-        // skips the rest if headless
-        //assumeFalse(guiRobot.isHeadlessMode());
+        terminal.inputCommand("client view 1");
+        String resultMessage3 = getResultMessage();
+        String expectedMessage3 = String.format(MESSAGE_VIEW_CLIENT_SUCCESS, "Lim");
+        assertEquals(expectedMessage3, resultMessage3);
 
         // viewing clients
         terminal.inputCommand("client view 1");
@@ -121,6 +129,7 @@ public class MainWindowTest extends GuiUnitTest {
 
         // viewing country
         terminal.inputCommand("country note view");
+        //TODO
         pauseToEyeball();
 
     }
@@ -149,7 +158,6 @@ public class MainWindowTest extends GuiUnitTest {
         }
 
         public void inputCommand(String command) {
-            guiRobot.clickOn("#commandTextField");
             textInputControl.setText(command);
             guiRobot.pauseForHuman();
             guiRobot.press(KeyCode.ENTER).release(KeyCode.ENTER);
