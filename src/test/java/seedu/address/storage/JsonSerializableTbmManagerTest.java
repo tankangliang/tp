2022ec39fa734d09c5c0611df63ce457aab56fdb
tankangliang <tankangliang98@gmail.com>
2020-11-02 01:39:20 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/storage/JsonSerializableTbmManagerTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableTbmManagerTest.java
@@ -5,17 +5,12 @@ import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.TbmManager;
-import seedu.address.model.country.Country;
-import seedu.address.model.note.CountryNote;
-import seedu.address.model.tag.Tag;
 import seedu.address.testutil.TypicalClients;
 
 public class JsonSerializableTbmManagerTest {
