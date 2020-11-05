762cf47f9ba69cf633b9f706227f268720565c02
Rayson Koh <rayson.rk.koh@gmail.com>
2020-11-05 12:13:08 +0800

    Add EP comments for CountryNoteAddParser

--- src/test/java/seedu/address/logic/parser/CountryNoteAddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CountryNoteAddCommandParserTest.java
@@ -23,12 +23,11 @@ public class CountryNoteAddCommandParserTest {
     private final CountryNoteAddCommandParser parser = new CountryNoteAddCommandParser(tagNoteMap);
 
     @Test
-    public void parse_noCountryNoNote_throwsParseException() {
+    public void parse() {
+        // EP: no country + no note -> parseException
         assertThrows(ParseException.class, () -> parser.parse(" random string"));
-    }
 
-    @Test
-    public void parse_withPreamable_throwsParseException() {
+        // EP: with preamble -> parseException
         assertThrows(ParseException.class, () -> parser.parse(" random string c/SG nt/abc t/a"));
         assertThrows(ParseException.class, () -> parser.parse(" random string c/SG nt/ t/"));
         assertThrows(ParseException.class, () -> parser.parse(" random string c/ nt/abc t"));
@@ -37,50 +36,40 @@ public class CountryNoteAddCommandParserTest {
         assertThrows(ParseException.class, () -> parser.parse(" 123 c/SG nt/"));
         assertThrows(ParseException.class, () -> parser.parse(" 123 c/ nt/abc"));
         assertThrows(ParseException.class, () -> parser.parse(" 123 c/ nt/"));
-    }
 
-    @Test
-    public void parse_hasCountryNoNote_throwsParseException() {
+        // EP: has country + no note -> parseException
         assertThrows(ParseException.class, () -> parser.parse(" c/SG random string"));
         assertThrows(ParseException.class, () -> parser.parse(" c/SG t/random string"));
         assertThrows(ParseException.class, () -> parser.parse(" c/SG t/"));
-    }
 
-    @Test
-    public void parse_invalidCountryHasNote_throwsParseException() {
+        // EP: invalid country + has note -> parseException
         assertThrows(ParseException.class, () -> parser.parse(" c/Z Z nt/random string"));
         assertThrows(ParseException.class, () -> parser.parse(" c/Z Z t/a nt/random string"));
         assertThrows(ParseException.class, () -> parser.parse(" c/RUS t/a nt/random string"));
         assertThrows(ParseException.class, () -> parser.parse(" c/R t/a nt/random string"));
-    }
 
-    @Test
-    public void parse_validCountryNoNote_throwsParseException() {
+        // EP: valid country + no note -> parseException
         assertThrows(ParseException.class, () -> parser.parse(" c/MY "));
         assertThrows(ParseException.class, () -> parser.parse(" c/MY t/a"));
         assertThrows(ParseException.class, () -> parser.parse(" c/MY nt/ t/a"));
         assertThrows(ParseException.class, () -> parser.parse(" nt/ c/MY t/a"));
-    }
 
-    @Test
-    public void parse_validCountryHasNoteNoUntagged_assertNotEquals() {
+        // EP: valid country + has note + no untagged -> false
+        CountryNote countryNote = new CountryNote("random string", new Country("SG"));
+        CountryNoteAddCommand wrongCommand = new CountryNoteAddCommand(countryNote);
         try {
-            CountryNote countryNote = new CountryNote("random string", new Country("SG"));
-            CountryNoteAddCommand wrongCommand = new CountryNoteAddCommand(countryNote);
             assertNotEquals(wrongCommand, parser.parse(" c/SG nt/random string"));
         } catch (ParseException e) {
             fail();
         }
-    }
 
-    @Test
-    public void parse_validCountryHasNoteHasUntagged_equalsExpected() {
+        // EP: valid country + has note + has untagged -> true
+        countryNote = new CountryNote("random string", new Country("SG"));
+        Set<Tag> tags = new HashSet<>();
+        tags.add(Tag.UNTAGGED);
+        countryNote.setTags(tags);
+        CountryNoteAddCommand expected = new CountryNoteAddCommand(countryNote);
         try {
-            CountryNote countryNote = new CountryNote("random string", new Country("SG"));
-            Set<Tag> tags = new HashSet<>();
-            tags.add(Tag.UNTAGGED);
-            countryNote.setTags(tags);
-            CountryNoteAddCommand expected = new CountryNoteAddCommand(countryNote);
             assertEquals(expected, parser.parse(" c/SG nt/random string"));
         } catch (ParseException e) {
             fail();
