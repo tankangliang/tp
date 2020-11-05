a6c09295922145f567742f4fd0e78b07e2f74aae
LeeEnHao <lienhao.le@gmail.com>
2020-11-05 15:10:47 +0800

    Remove unused import

--- src/test/java/seedu/address/logic/parser/CountryNoteDeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CountryNoteDeleteCommandParserTest.java
@@ -2,7 +2,6 @@ package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
