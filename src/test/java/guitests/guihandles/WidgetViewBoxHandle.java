c5fd48fd16a4b924f81f3311221224ab0f1d4ccc
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-11-07 00:58:28 +0800

    Update gui tests

--- src/test/java/guitests/guihandles/WidgetViewBoxHandle.java
+++ src/test/java/guitests/guihandles/WidgetViewBoxHandle.java
@@ -1,8 +1,11 @@
 package guitests.guihandles;
 
 import javafx.scene.Node;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import seedu.address.model.client.Client;
+import seedu.address.ui.NoteListCard;
 
 /**
  * Provides a handler to the widget view box.
@@ -14,17 +17,17 @@ public class WidgetViewBoxHandle extends NodeHandle<Node> {
     private static final String ADDRESS_FIELD_ID = "#address";
     private static final String COUNTRY_FIELD_ID = "#country";
     private static final String CONTRACT_EXPIRY_DATE_FIELD_ID = "#contractExpiryDate";
+    private static final String NOTE_LIST_VIEW_ID = "#clientNoteListView";
     private final Text nameLabel;
     private final Text phoneLabel;
     private final Text emailLabel;
     private final Text addressLabel;
     private final Text countryLabel;
     private final Text contractExpiryDateLabel;
+    private final VBox noteListView;
 
     /**
      * Constructor for handler.
-     *
-     * @param widgetNode
      */
     public WidgetViewBoxHandle(Node widgetNode) {
         super(widgetNode);
@@ -35,19 +38,32 @@ public class WidgetViewBoxHandle extends NodeHandle<Node> {
         addressLabel = getChildNode(ADDRESS_FIELD_ID);
         countryLabel = getChildNode(COUNTRY_FIELD_ID);
         contractExpiryDateLabel = getChildNode(CONTRACT_EXPIRY_DATE_FIELD_ID);
+        noteListView = getChildNode(NOTE_LIST_VIEW_ID);
     }
 
     /**
      * Returns true if this handle contains a {@code client}.
      */
     public boolean equals(Client client) {
+        boolean notesEqual = noteListView.getChildren().size() == client.getClientNotesAsObservableList().size();
+        for (int i = 0; i < noteListView.getChildren().size(); i++) {
+            VBox noteListCard = (VBox) noteListView.getChildren().get(i);
+            TextFlow noteTextFlow = (TextFlow) noteListCard.getChildren().get(1);
+            Text noteText = (Text) noteTextFlow.getChildren().get(0);
+            String noteContent = client.getClientNotesAsObservableList().get(i).getNoteContent();
+            if (!noteText.getText().equals(noteContent)) {
+                notesEqual = false;
+                break;
+            }
+        }
         return nameLabel.getText().equals(client.getName().toString())
                 && phoneLabel.getText().equals("Phone: " + client.getPhone().toString())
                 && emailLabel.getText().equals("Email: " + client.getEmail().toString())
                 && addressLabel.getText().equals("Address: " + client.getAddress().toString())
                 && countryLabel.getText()
                         .equals(client.getCountry().getCountryName() + " (" + client.getTimezone().toString() + ")")
-                && contractExpiryDateLabel.getText().equals("Expiry: " + client.getContractExpiryDate().displayValue);
+                && contractExpiryDateLabel.getText().equals("Expiry: " + client.getContractExpiryDate().displayValue)
+                && notesEqual;
     }
 
 }
