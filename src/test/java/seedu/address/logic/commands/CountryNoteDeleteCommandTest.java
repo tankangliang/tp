000b010c9e8a3032460d4eaf7f0d032f864fdc64
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-11-07 16:02:08 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/CountryNoteDeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CountryNoteDeleteCommandTest.java
@@ -1,10 +1,8 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TestUtil.basicEqualsTests;
