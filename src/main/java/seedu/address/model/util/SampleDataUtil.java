ffd895d4cbcfa5d1d9259aeee415c295ca37c7d7
LeeEnHao <lienhao.le@gmail.com>
2020-11-08 15:20:07 +0800

    Fix trailing whitespace

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -89,7 +89,6 @@ public class SampleDataUtil {
         alex.addClientNote(alexNote3);
         alex.addClientNote(alexNote4);
         alex.addClientNote(alexNote5);
-      
         return new Client[]{alex, bernice, charlotte, david, irfan, roy};
     }
 
